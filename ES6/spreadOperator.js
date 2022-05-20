2f999afbf700f4f48e2c7c472542ce1bcb3300eb
Yun-Han <yhanh999@gmail.com>
2022-05-20 22:39:47 +0800

    using Spread Operator 展開運算子

--- ES6/spreadOperator.js
+++ ES6/spreadOperator.js
@@ -1,7 +1,7 @@
 // console.log(Math.max(-2, 6, 3, 1));   // 可以放入任意數量的 parameter
 
 function checkBiggest(...numbers) {
-    console.log(numbers);
+  console.log(numbers);
   let biggest = -Infinity;
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > biggest) {
@@ -13,3 +13,11 @@ function checkBiggest(...numbers) {
 console.log(checkBiggest(-2, 100, 6, 7, 10, 15, 18, 20, 29));
 
 console.log(Math.max(-2, 100, 6, 7, 10, 15, 18, 20, 29));
+
+// ------------------------------------------------------------
+
+let friends1 = ["Karla", "Russell", "Maegan"];
+let friends2 = ["Sam", "Trace", "Nathan"];
+
+let friends = [...friends1, ...friends2];
+console.log(friends); // output: (6) ['Karla', 'Russell', 'Maegan', 'Sam', 'Trace', 'Nathan']
