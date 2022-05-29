94f1176e95d6393bf37a124a9c61d59325e0af94
Yun-Han <yhanh999@gmail.com>
2022-05-29 21:22:27 +0800

    create a server by using only Node.js

--- Node.js/web-server/server.js
+++ Node.js/web-server/server.js
@@ -1,14 +1,22 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((request, response) => {
   // 客戶端的請求是甚麼，我要回復的 response 是甚麼
 
   //   console.log(request.url);
+
   //   請求的時候網址是一個斜線 "/"
   if (request.url == "/") {
-    response.write("You are on the homepage.");
-    response.end();
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      response.writeHead(200, { "Context-Type": "text/html" });
+      response.write(data);
+      response.end;
+    });
+    // response.write("You are on the homepage.");
+    // response.end();
   } else {
     let parsedURL = url.parse(request.url);
 
