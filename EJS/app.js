cbef83355a03c24ed868b75b9d485b81f0369378
yhanh <yhanh999@gmail.com>
2022-06-05 10:41:28 +0800

    array data in EJS

--- EJS/app.js
+++ EJS/app.js
@@ -7,7 +7,14 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //   res.send("This is homepage.");
-  res.render("index.ejs"); // 要被 res.render 的文件一定要再 views 資料夾中
+  const languages = [
+    { name: "Python", rating: 9.5, popularity: 9.7, trending: "super hot" },
+    { name: "Java", rating: 8.6, popularity: 8.2, trending: "same" },
+    { name: "C++", rating: 6.6, popularity: 7.7, trending: "same" },
+    { name: "PHP", rating: 2.5, popularity: 4.7, trending: "decreasing" },
+    { name: "JavaScript", rating: 8.5, popularity: 8.1, trending: "same" },
+  ];
+  res.render("index.ejs", { languages }); // 要被 res.render 的文件一定要再 views 資料夾中
 });
 
 // app.get("/:name", (req, res) => {
